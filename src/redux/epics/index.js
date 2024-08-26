999d2804af023bac9cb6c3d6a2382571d4cec7e9
Ashley.Wright <ashley.wright@mx.com>
2024-08-26 10:44:32 -0600

    fixed lint issues

--- src/redux/epics/index.js
+++ src/redux/epics/index.js
@@ -1,8 +1,8 @@
 import { combineEpics } from 'redux-observable'
 
-import { loadConnect, selectInstitution } from './Connect'
+import { loadConnect, selectInstitution } from 'src/redux/epics/Connect'
 // import { loadConnections, updateAccountsAfterConnecting } from './Connections'
-import { postMessages } from './PostMessage'
+import { postMessages } from 'src/redux/epics/PostMessage'
 
 export const rootEpic = combineEpics(
   loadConnect,
