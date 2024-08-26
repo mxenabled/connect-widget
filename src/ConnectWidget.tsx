c6a6e55f6ad0cb1fb547aa192c5b6ccd33590117
Clement Mwimo <clement.mwimo@mx.com>
2024-08-26 13:02:36 -0600

    set profiles and user features

--- src/ConnectWidget.tsx
+++ src/ConnectWidget.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react'
-import { Provider } from 'react-redux'
+import React, { useEffect } from 'react'
+import { Provider, useDispatch } from 'react-redux'
 
 import Store from 'src/redux/Store'
 import Connect from 'src/Connect'
+import { loadProfiles } from 'src/redux/reducers/profilesSlice'
+import { loadUserFeatures } from 'src/redux/reducers/userFeaturesSlice'
 
 export const ConnectWidget = (props: any) => {
+  const { profiles, userFeatures, ...rest } = props
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(loadProfiles(profiles))
+    dispatch(loadUserFeatures(userFeatures))
+  }, [profiles, userFeatures])
+
   return (
     <Provider store={Store}>
-      <Connect {...props} />
+      <Connect {...rest} />
     </Provider>
   )
 }
