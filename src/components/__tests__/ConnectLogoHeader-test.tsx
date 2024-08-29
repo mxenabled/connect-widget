82719a1687d5ef1a1465983fef10eac9489384d3
Clement Mwimo <clement.mwimo@mx.com>
2024-08-29 13:32:11 -0600

    fixed connectLogoHeader test

--- src/components/__tests__/ConnectLogoHeader-test.tsx
+++ src/components/__tests__/ConnectLogoHeader-test.tsx
@@ -1,44 +1,34 @@
 import React from 'react'
-import { Store } from 'redux'
-import { render, screen, createTestReduxStore } from 'src/utilities/testingLibrary'
-
-import { ActionTypes as ClientActionTypes } from 'src/redux/actions/Client'
-import { loadProfiles } from 'src/redux/reducers/profilesSlice'
+import { render, screen } from 'src/utilities/testingLibrary'
 import { ConnectLogoHeader } from 'src/components/ConnectLogoHeader'
 
 describe('ConnectLogoHeader', () => {
   const clientGuid = 'CLT-123'
-  let store: Store
-
-  beforeEach(() => {
-    store = createTestReduxStore()
-    store.dispatch({ type: ClientActionTypes.INITIALIZED_CLIENT_CONFIG })
-    store.dispatch(
-      loadProfiles({
-        client: {
-          default_institution_guid: 'HOMEACCOUNT',
-          guid: clientGuid,
-          name: 'Client Name',
-        },
-        client_color_scheme: {
-          primary_100: '',
-          primary_200: '',
-          primary_300: '',
-          primary_400: '',
-          primary_500: '',
-          color_scheme: 'light',
-          widget_brand_color: '',
-        },
-        client_profile: {},
-        user: {},
-        user_profile: {},
-        widget_profile: {},
-      }),
-    )
-  })
+  const initialState = {
+    profiles: {
+      client: {
+        default_institution_guid: 'HOMEACCOUNT',
+        guid: clientGuid,
+        name: 'Client Name',
+      },
+      clientColorScheme: {
+        primary_100: '',
+        primary_200: '',
+        primary_300: '',
+        primary_400: '',
+        primary_500: '',
+        color_scheme: 'light',
+        widget_brand_color: '',
+      },
+      clientProfile: {},
+      user: {},
+      userProfile: {},
+      widgetProfile: {},
+    },
+  }
 
   it('renders a ClientLogo', () => {
-    render(<ConnectLogoHeader />, { store })
+    render(<ConnectLogoHeader />, { preloadedState: initialState })
     const img = screen.getByAltText('Client logo')
     expect(img.getAttribute('src')).toEqual(
       `https://content.moneydesktop.com/storage/MD_Client/oauth_logos/${clientGuid}.png`,
@@ -47,7 +37,9 @@ describe('ConnectLogoHeader', () => {
 
   it('renders an InstitutionLogo if an institutionGuid was passed in props', () => {
     const institutionGuid = 'INS-123'
-    render(<ConnectLogoHeader institutionGuid={institutionGuid} />, { store })
+    render(<ConnectLogoHeader institutionGuid={institutionGuid} />, {
+      preloadedState: initialState,
+    })
 
     const img = screen.getByAltText('Institution logo')
     expect(img.getAttribute('src')).toEqual(
@@ -56,7 +48,7 @@ describe('ConnectLogoHeader', () => {
   })
 
   it('renders two SVGImage if an institutionGuid is not passed in props', () => {
-    render(<ConnectLogoHeader />, { store })
+    render(<ConnectLogoHeader />, { preloadedState: initialState })
     expect(screen.queryAllByTestId('svg-image').length).toBe(2)
   })
 })
