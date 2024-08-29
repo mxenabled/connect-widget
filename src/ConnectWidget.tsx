eb8444801635960715daae4efc8d184b2093fe9c
Jameson Brown <jameson.brown@mx.com>
2024-08-29 15:13:29 -0600

    Added translation utils and scripts

--- src/ConnectWidget.tsx
+++ src/ConnectWidget.tsx
@@ -5,8 +5,11 @@ import { Provider } from 'react-redux'
 import Store from 'src/redux/Store'
 import Connect from 'src/Connect'
 import { WidgetDimensionObserver } from 'src/components/app/WidgetDimensionObserver'
+import { initGettextLocaleData } from 'src/utilities/Personalization'
 
 export const ConnectWidget = (props: any) => {
+  initGettextLocaleData(props.language)
+
   return (
     <Provider store={Store}>
       <WidgetDimensionObserver heightOffset={0}>
