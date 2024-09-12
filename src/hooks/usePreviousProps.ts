921714fa31bffb35fd3fd7d19d68060add2758d0
Clement Mwimo <clement.mwimo@mx.com>
2024-09-12 17:46:05 -0600

    used function instead

--- src/hooks/usePreviousProps.ts
+++ src/hooks/usePreviousProps.ts
@@ -1,10 +1,10 @@
 import { useEffect, useRef } from 'react'
 
-const usePreviousProps = <T>(props: T): T => {
+function usePreviousProps<T>(props: T): T {
   const prevPropsRef = useRef(props)
   useEffect(() => {
     prevPropsRef.current = props
-  })
+  }, [])
   return prevPropsRef.current
 }
 
