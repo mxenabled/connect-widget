b2e0f45ff7ed3a53282c2d6cfb2cece7bcd4f2ba
Ashley.Wright <ashley.wright@mx.com>
2024-09-20 16:52:42 -0600

    added some api changes to the happy path, WIP

--- typings/mxTypes.d.ts
+++ typings/mxTypes.d.ts
@@ -8,11 +8,26 @@ type AccountType = {
 }
 
 // Member types
-type MemberType = { guid: string }
+type MemberType = {
+  guid: string
+  connection_status: number
+}
 
 // Institution types
-type InstitutionType = { guid: string }
-
+type InstitutionType = {
+  account_verification_is_enabled: boolean
+  code: string
+  forgot_password_credential_recovery_url: string
+  forgot_username_credential_recovery_url: string
+  guid: string
+  login_url: string
+  name: string
+  popularity: number
+  supports_oauth: boolean
+  tax_statement_is_enabled: boolean
+  trouble_signing_credential_recovery_url: string
+  url: string
+}
 // Microdeposit types
 type MicrodepositCreateType = {
   guid: string
@@ -40,4 +55,18 @@ type MicrodepositResponseType = {
 type MicroDepositVerifyResponseType = {
   guid: string
   status: string
+}
+type OAuthWindowURIType = {
+  guid: string
+  oauth_window_uri: string
+}
+type CredentialType = {
+  display_order: 1
+  field_name: string
+  field_type: number
+  guid: string
+  label: string
+  meta_data: object
+  optional: boolean
+  options: object
 }
