3ff103734eedd15b5909b1e03b407dd1e78c4249
Jameson Brown <jameson.brown@mx.com>
2024-09-20 14:37:00 -0600

    Added mx types declaration file

---
+++ typings/mxTypes.d.ts
@@ -0,0 +1,13 @@
+type AccountType = {
+  account_type: number
+  balance: number
+  guid: string
+  interest_rate: number
+  is_personal: boolean
+}
+type MemberType = { guid: string }
+type InstitutionType = { guid: string }
+type MicroDepositType = {
+  guid: string
+  status: string
+}
