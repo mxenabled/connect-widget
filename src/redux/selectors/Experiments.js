585c84bbbbde2dd8d2fd8a1283b11dbbe949e902
Jameson Brown <jameson.brown@mx.com>
2024-10-11 12:01:36 -0600

    cleaned up experiements selectors

--- src/redux/selectors/Experiments.js
+++ src/redux/selectors/Experiments.js
@@ -1,9 +1,6 @@
 import { createSelector } from 'reselect'
 
 export const getExperiments = (state) => state.experiments.items
-export const getActiveExperiments = createSelector(getExperiments, (experiments) =>
-  experiments.filter((experiment) => experiment.is_active),
-)
 
 /*
   This generates an object that references the name of each experiment to the experiments selected variant.
@@ -12,20 +9,17 @@ export const getActiveExperiments = createSelector(getExperiments, (experiments)
     ...
   }
 */
-export const getExperimentNamesToUserVariantMap = createSelector(
-  getActiveExperiments,
-  (experiments) => {
-    return experiments.reduce((acc, experiment) => {
-      if (experiment.selected_variant) {
-        const selectedVariantFeatureGuid = experiment.selected_variant.user_feature.feature_guid
-        // get the variant name
-        const feature = experiment.features.find(
-          (feature) => feature.guid === selectedVariantFeatureGuid,
-        )
+export const getExperimentNamesToUserVariantMap = createSelector(getExperiments, (experiments) => {
+  return experiments.reduce((acc, experiment) => {
+    if (experiment.is_active && experiment.selected_variant) {
+      const selectedVariantFeatureGuid = experiment.selected_variant.user_feature.feature_guid
+      // get the variant name
+      const feature = experiment.features.find(
+        (feature) => feature.guid === selectedVariantFeatureGuid,
+      )
 
-        return { ...acc, [experiment.name]: feature.name }
-      }
-      return acc
-    }, {})
-  },
-)
+      return { ...acc, [experiment.name]: feature.name }
+    }
+    return acc
+  }, {})
+})
