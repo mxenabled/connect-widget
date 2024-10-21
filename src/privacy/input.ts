0c5a2afccf40aaee4afb4cf09c019bb71d557cbf
Jameson Brown <jameson.brown@mx.com>
2024-10-21 15:46:48 -0600

    Removed unused kyper components

--- src/privacy/input.ts
+++ src/privacy/input.ts
@@ -1,11 +1,10 @@
 /* eslint-disable no-restricted-imports */
 // This is the ONLY file that @kyper related inputs should be directly imported
 
-import { PasswordInput, Radio, Switch, InputGroup, PASSWORD_VALIDATIONS } from '@kyper/input'
+import { PasswordInput, Radio, PASSWORD_VALIDATIONS } from '@kyper/input'
 import TextField from '@mui/material/TextField'
 import { Select } from '@kyper/select'
 import { SelectionBox } from '@kyper/selectionbox'
-import { TextArea } from '@kyper/textarea'
 import { UserFeedback } from '@kyper/userfeedback'
 import { withProtection } from 'src/privacy/withProtection'
 
@@ -23,22 +22,16 @@ import { withProtection } from 'src/privacy/withProtection'
 const ProtectedTextField = withProtection(TextField)
 const ProtectedPasswordInput = withProtection(PasswordInput)
 const ProtectedRadio = withProtection(Radio)
-const ProtectedSwitch = withProtection(Switch)
-const ProtectedInputGroup = withProtection(InputGroup)
 const ProtectedSelect = withProtection(Select)
 const ProtectedSelectionBox = withProtection(SelectionBox)
-const ProtectedTextArea = withProtection(TextArea)
 const ProtectedUserFeedback = withProtection(UserFeedback)
 
 export {
   ProtectedTextField as TextField,
   ProtectedPasswordInput as PasswordInput,
   ProtectedRadio as Radio,
-  ProtectedSwitch as Switch,
-  ProtectedInputGroup as InputGroup,
   ProtectedSelect as Select,
   ProtectedSelectionBox as SelectionBox,
-  ProtectedTextArea as TextArea,
   ProtectedUserFeedback as UserFeedback,
   PASSWORD_VALIDATIONS as PasswordValidations,
 }
