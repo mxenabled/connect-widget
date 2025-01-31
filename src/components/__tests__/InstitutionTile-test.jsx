4540111c75141ce9d802f3a4fe77c28597b485c2
Wes Risenmay <wes.risenmay@mx.com>
2025-01-31 12:55:29 -0700

    added some tests for the logoUrl changes

---
+++ src/components/__tests__/InstitutionTile-test.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render, screen } from 'src/utilities/testingLibrary'
+import { InstitutionTile } from '../InstitutionTile'
+
+describe('<InstitutionTile />', () => {
+  it('renders the logoUrl in the src if there is one', () => {
+    const institution = {
+      name: 'testName',
+      logo_url: 'testLogoUrl',
+    }
+
+    render(<InstitutionTile institution={institution} selectInstitution={() => {}} />)
+
+    expect(screen.getByRole('img')).toHaveAttribute('src', institution.logo_url)
+  })
+
+  it('renders a generated url with the guid if there is no logoUrl', () => {
+    const institution = {
+      guid: 'testGuid',
+      name: 'testName',
+    }
+
+    render(<InstitutionTile institution={institution} selectInstitution={() => {}} />)
+
+    expect(screen.getByRole('img').src.includes(institution.guid)).toBe(true)
+  })
+})
