863c75af28541db598caae48c21c9280f3551320
Clement Mwimo <clement.mwimo@mx.com>
2025-03-28 01:47:37 -0600

    fixed styles

--- src/components/ConfigError.tsx
+++ src/components/ConfigError.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Text } from '@kyper/mui'
+import { useTokens } from '@kyper/tokenprovider'
 import { Container } from 'src/components/Container'
+import { AttentionFilled } from '@kyper/icon/AttentionFilled'
 
 interface ConfigError {
   title: string
@@ -13,16 +15,37 @@ interface ConfigErrorProps {
 }
 
 export const ConfigError: React.FC<ConfigErrorProps> = ({ error }) => {
+  const tokens = useTokens()
+  const styles = getStyles(tokens)
   return (
     <Container>
-      <div>
+      <div style={styles.container}>
+        <AttentionFilled color="#4D4D4D" size={32} style={styles.errorIcon} />
         <Text component={'h2'} truncate={false} variant="H2">
           {error.title}
         </Text>
-        <Text component="p" truncate={false} variant="ParagraphSmall">
+        <Text component="p" truncate={false} variant="Paragraph">
           {error.message}
         </Text>
       </div>
     </Container>
   )
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getStyles = (tokens: any) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyCcontent: 'center',
+    marginTop: '36px',
+    textAlign: 'center',
+  } as React.CSSProperties,
+  errorTitle: {
+    marginBottom: tokens.Spacing.Tiny,
+  },
+  errorIcon: {
+    marginBottom: tokens.Spacing.Large,
+  },
+})
