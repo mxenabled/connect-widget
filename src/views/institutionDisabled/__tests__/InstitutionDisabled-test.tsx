c263ce42a5d2159c415902f70a5242aac9febab0
Jameson Brown <jameson.brown@mx.com>
2025-08-14 14:16:58 -0600

    Updated tests to reflect less props

--- src/views/institutionDisabled/__tests__/InstitutionDisabled-test.tsx
+++ src/views/institutionDisabled/__tests__/InstitutionDisabled-test.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { ActionTypes } from 'src/redux/actions/Connect'
+import { initialState } from 'src/redux/reducers/configSlice'
 import { render, screen, waitFor } from 'src/utilities/testingLibrary'
 import { InstitutionDisabled } from 'src/views/institutionDisabled/InstitutionDisabled'
+
+// Mock useDispatch
+vitest.mock('react-redux', async () => {
+  const actual = await vitest.importActual('react-redux')
+  return { ...actual, useDispatch: vitest.fn() }
+})
+const mockDispatch = vitest.fn()
+const mockedUseDispatch = vitest.mocked(useDispatch)
 
 describe('InstitutionDisabled', () => {
   const preloadedState = {
@@ -10,16 +21,20 @@ describe('InstitutionDisabled', () => {
         guid: 'INS-123',
       },
     },
+    config: {
+      ...initialState,
+      _initialValues: JSON.stringify(initialState),
+    },
   }
-  const mockOnGoBackClick = vitest.fn()
 
   beforeEach(() => {
-    render(<InstitutionDisabled onGoBackClick={mockOnGoBackClick} />, {
+    // Reset the mock before each test
+    mockDispatch.mockClear()
+    mockedUseDispatch.mockReturnValue(mockDispatch)
+
+    render(<InstitutionDisabled />, {
       preloadedState,
     })
-  })
-  afterEach(() => {
-    mockOnGoBackClick.mockClear()
   })
 
   it('renders institution logo with disabled icon', () => {
@@ -47,6 +62,13 @@ describe('InstitutionDisabled', () => {
 
     expect(button).toBeInTheDocument()
     button.click()
-    waitFor(() => expect(mockOnGoBackClick).toHaveBeenCalledTimes(1))
+    waitFor(() =>
+      expect(mockDispatch).toHaveBeenCalledWith({
+        type: ActionTypes.GO_BACK_INSTITUTION_DISABLED,
+        payload: {
+          mode: 'AGG',
+        },
+      }),
+    )
   })
 })
