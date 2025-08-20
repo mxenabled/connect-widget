48aa677acc2b66e7a526083a2a005fa6f8b32542
Wes Risenmay <wes.risenmay@mx.com>
2025-08-20 09:04:34 -0600

    converting to css modules

--- src/views/connecting/progress/ProgressBackgroundImage.tsx
+++ src/views/connecting/progress/ProgressBackgroundImage.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { COLOR_SCHEME } from 'src/const/Connect'
-import { selectColorScheme } from 'src/redux/reducers/configSlice'
+import { getIsLightColorScheme } from 'src/redux/reducers/configSlice'
 import ProgressBackdropDark from './ProgressBackdropDark.svg'
 import ProgressBackdropLight from './ProgressBackdropLight.svg'
 
-export const ProgressBackgroundImage = () => {
-  const colorScheme = useSelector(selectColorScheme)
+export const ProgressBackgroundImage = ({ className }: { className: string }) => {
+  const isLightColorScheme = useSelector(getIsLightColorScheme)
 
-  return colorScheme === COLOR_SCHEME.LIGHT ? <ProgressBackdropLight /> : <ProgressBackdropDark />
+  return (
+    <div className={className}>
+      {isLightColorScheme ? <ProgressBackdropLight /> : <ProgressBackdropDark />}
+    </div>
+  )
 }
--- src/views/connecting/progress/ProgressBar.tsx
+++ src/views/connecting/progress/ProgressBar.tsx
@@ -15,6 +15,7 @@ import { ProgressLogo } from './ProgressLogo'
 import { InstitutionLogo } from '@mxenabled/mxui'
 import { Stack } from '@mui/material'
 import { ProgressBackgroundImage } from './ProgressBackgroundImage'
+import styles from './progressBar.module.css'
 
 export const ProgressBar = ({
   institution,
@@ -27,40 +28,10 @@ export const ProgressBar = ({
 
   const clientGuid = useSelector(getClientGuid)
 
-  const logoPadding = '28px'
-  const backgroundImageHeight = '80px'
-
-  const styles = {
-    backgroundImage: {
-      height: backgroundImageHeight,
-      width: backgroundImageHeight,
-      zIndex: 1,
-    },
-    container: {
-      textAlign: 'center',
-    },
-    barContainer: {
-      alignItems: 'center',
-      display: 'flex',
-      height: backgroundImageHeight,
-      justifyContent: 'center',
-    },
-    logo: { border: 'solid 1px rgba(0, 0, 0, 0.25)', borderRadius: '8px' },
-    logosContainer: {
-      alignItems: 'center',
-      display: 'flex',
-      justifyContent: 'space-between',
-      paddingLeft: logoPadding,
-      paddingRight: logoPadding,
-      position: 'absolute',
-      width: `calc(100% - ${logoPadding} - ${logoPadding})`,
-    },
-  } as const
-
   // if we don't have the schedule initialized just show a spinner.
   if (jobSchedule.isInitialized === false) {
     return (
-      <div style={styles.container}>
+      <div className={styles.container}>
         <Spinner bgColor="transparent" fgColor={tokens.Color.Brand300} />
       </div>
     )
@@ -69,22 +40,26 @@ export const ProgressBar = ({
   const activeJob = JobSchedule.getActiveJob(jobSchedule)
 
   return (
-    <Stack spacing="32px" style={styles.container}>
-      <div style={styles.barContainer}>
-        <div style={styles.logosContainer}>
+    <Stack className={styles.container} spacing="32px">
+      <div className={styles.barContainer}>
+        <div className={styles.logosContainer}>
           <ProgressLogo>
-            <ClientLogo alt="Client logo" clientGuid={clientGuid} size={64} style={styles.logo} />
+            <ClientLogo
+              alt="Client logo"
+              className={styles.logo}
+              clientGuid={clientGuid}
+              size={64}
+            />
           </ProgressLogo>
-          <div data-testId="mxLogo" style={styles.backgroundImage}>
-            <ProgressBackgroundImage />
-          </div>
+          {/* Do we actually need this testId? */}
+          <ProgressBackgroundImage className={styles.backgroundImage} data-testId="mxLogo" />
           <ProgressLogo>
             <InstitutionLogo
               alt="Institution logo"
+              className={styles.logo}
               institutionGuid={institution.guid}
               logoUrl={institution.logo_url}
               size={64}
-              style={styles.logo}
             />
           </ProgressLogo>
         </div>
