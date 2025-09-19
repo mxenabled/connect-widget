e74e49902aebd90215cb48295718902158965f0f
Jameson Brown <jameson.brown@mx.com>
2025-09-19 11:10:18 -0600

    feat(actionableerror): refactor ActionableError component and add useActionableErrorMap
    Refactor ActionableError component and add useActionableErrorMap for error handling; enhance error
    codes in consts
    
    CT-1711

--- src/views/actionableError/consts.ts
+++ src/views/actionableError/consts.ts
@@ -1,15 +1,34 @@
 import { VERIFY_MODE } from 'src/const/Connect'
 
 // AED Step 1: Add new codes here
-// AED Step 2: see ActionableError.tsx
+// AED Step 2: see useActionableErrorMap.tsx
 export const ACTIONABLE_ERROR_CODES = {
   NO_ELIGIBLE_ACCOUNTS: 1000,
+  NO_ACCOUNTS: 1001,
+  ACCESS_DENIED: 1002,
+  INCORRECT_OTP: 2001,
+  INCORRECT_MFA: 2002,
+  MFA_TIMEOUT: 2003,
+  REAUTHORIZATION_REQUIRED: 2004,
+  INVALID_CREDENTIALS: 2005,
+  INSTITUTION_DOWN: 3000,
+  INSTITUTION_MAINTENANCE: 3001,
+  INSTITUTION_UNAVAILABLE: 3002,
 }
+export const CODES_REQUIRING_CREDENTIALS = [
+  ACTIONABLE_ERROR_CODES.INVALID_CREDENTIALS,
+  ACTIONABLE_ERROR_CODES.REAUTHORIZATION_REQUIRED,
+]
+export const CODES_REQUIRING_MFA = [
+  ACTIONABLE_ERROR_CODES.INCORRECT_OTP,
+  ACTIONABLE_ERROR_CODES.INCORRECT_MFA,
+  ACTIONABLE_ERROR_CODES.MFA_TIMEOUT,
+]
 
 export const ACTIVE_ACTIONABLE_ERROR_CODES: number[] = Object.values(ACTIONABLE_ERROR_CODES)
 
 export const canHandleActionableError = (errorCode: number, mode: string) => {
-  // There are some scenarios where the actionable error should not be shown
+  // VALIDATION: There are some scenarios where the actionable error should not be shown
   // For example, NO_ELIGIBLE_ACCOUNTS(1000) should only be shown in VERIFY mode
   // because that error code is only applicable in VERIFY jobs.
   if (errorCode === ACTIONABLE_ERROR_CODES.NO_ELIGIBLE_ACCOUNTS && mode !== VERIFY_MODE) {
---
+++ src/views/actionableError/useActionableErrorMap.tsx
@@ -0,0 +1,76 @@
+import { useContext, useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { PostMessageContext } from 'src/ConnectWidget'
+import { ActionTypes } from 'src/redux/actions/Connect'
+import { ACTIONABLE_ERROR_CODES } from 'src/views/actionableError/consts'
+import { __ } from 'src/utilities/Intl'
+import { RootState } from 'src/redux/Store'
+
+type ActionableErrorAction = {
+  label: string
+  action: () => void
+}
+type ActionableErrorMapEntry = {
+  title: string
+  primaryAction: ActionableErrorAction
+  secondaryActions: ActionableErrorAction
+}
+
+export const useActionableErrorMap = (
+  jobDetailCode: string,
+  setShowSupport: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
+  const postMessageFunctions = useContext(PostMessageContext)
+  const initialConfig = useSelector((state: RootState) => state.connect.initialConfig)
+  const dispatch = useDispatch()
+
+  // Action options for mapping below
+  const goToSearch = () => {
+    postMessageFunctions.onPostMessage('connect/backToSearch')
+    dispatch({
+      type: ActionTypes.ACTIONABLE_ERROR_CONNECT_DIFFERENT_INSTITUTION,
+      payload: initialConfig.mode,
+    })
+  }
+  const gotToSupport = () => setShowSupport(true)
+  const gotToCredentials = () => dispatch({ type: ActionTypes.ACTIONABLE_ERROR_LOG_IN_AGAIN })
+
+  // AED Step 3: Add code mapping for new codes here
+  const messagingMap: Record<string, ActionableErrorMapEntry> = useMemo(
+    () => ({
+      [ACTIONABLE_ERROR_CODES.NO_ELIGIBLE_ACCOUNTS]: {
+        title: __('No eligible accounts'),
+        primaryAction: { label: __('Log in again'), action: gotToCredentials },
+        secondaryActions: { label: __('Connect a different institution'), action: goToSearch },
+      },
+      [ACTIONABLE_ERROR_CODES.NO_ACCOUNTS]: {
+        title: __('No accounts found'),
+        primaryAction: { label: __('Return to institution selection'), action: goToSearch },
+        secondaryActions: { label: __('Get help'), action: gotToSupport },
+      },
+      [ACTIONABLE_ERROR_CODES.ACCESS_DENIED]: {
+        title: __('Additional permissions needed'),
+        primaryAction: { label: __('Review instructions'), action: gotToCredentials },
+        secondaryActions: { label: __('Get help'), action: gotToSupport },
+      },
+      [ACTIONABLE_ERROR_CODES.INSTITUTION_DOWN]: {
+        title: __('Unable to connect'),
+        primaryAction: { label: __('Return to institution selection'), action: goToSearch },
+        secondaryActions: { label: __('Get help'), action: gotToSupport },
+      },
+      [ACTIONABLE_ERROR_CODES.INSTITUTION_MAINTENANCE]: {
+        title: __('Maintenance in progress'),
+        primaryAction: { label: __('Return to institution selection'), action: goToSearch },
+        secondaryActions: { label: __('Get help'), action: gotToSupport },
+      },
+      [ACTIONABLE_ERROR_CODES.INSTITUTION_UNAVAILABLE]: {
+        title: __('Unable to connect'),
+        primaryAction: { label: __('Return to institution selection'), action: goToSearch },
+        secondaryActions: { label: __('Get help'), action: gotToSupport },
+      },
+    }),
+    [dispatch],
+  )
+
+  return messagingMap[jobDetailCode]
+}
