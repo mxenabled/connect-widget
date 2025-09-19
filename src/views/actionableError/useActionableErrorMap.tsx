619c485424339c800abaf40808df320dfb51ffd6
Jameson Brown <jameson.brown@mx.com>
2025-09-19 14:22:02 -0600

    Fixed typo in function names

--- src/views/actionableError/useActionableErrorMap.tsx
+++ src/views/actionableError/useActionableErrorMap.tsx
@@ -32,41 +32,41 @@ export const useActionableErrorMap = (
       payload: initialConfig.mode,
     })
   }
-  const gotToSupport = () => setShowSupport(true)
-  const gotToCredentials = () => dispatch({ type: ActionTypes.ACTIONABLE_ERROR_LOG_IN_AGAIN })
+  const goToSupport = () => setShowSupport(true)
+  const goToCredentials = () => dispatch({ type: ActionTypes.ACTIONABLE_ERROR_LOG_IN_AGAIN })
 
   // AED Step 3: Add code mapping for new codes here
   const messagingMap: Record<string, ActionableErrorMapEntry> = useMemo(
     () => ({
       [ACTIONABLE_ERROR_CODES.NO_ELIGIBLE_ACCOUNTS]: {
         title: __('No eligible accounts'),
-        primaryAction: { label: __('Log in again'), action: gotToCredentials },
+        primaryAction: { label: __('Log in again'), action: goToCredentials },
         secondaryActions: { label: __('Connect a different institution'), action: goToSearch },
       },
       [ACTIONABLE_ERROR_CODES.NO_ACCOUNTS]: {
         title: __('No accounts found'),
         primaryAction: { label: __('Return to institution selection'), action: goToSearch },
-        secondaryActions: { label: __('Get help'), action: gotToSupport },
+        secondaryActions: { label: __('Get help'), action: goToSupport },
       },
       [ACTIONABLE_ERROR_CODES.ACCESS_DENIED]: {
         title: __('Additional permissions needed'),
-        primaryAction: { label: __('Review instructions'), action: gotToCredentials },
-        secondaryActions: { label: __('Get help'), action: gotToSupport },
+        primaryAction: { label: __('Review instructions'), action: goToCredentials },
+        secondaryActions: { label: __('Get help'), action: goToSupport },
       },
       [ACTIONABLE_ERROR_CODES.INSTITUTION_DOWN]: {
         title: __('Unable to connect'),
         primaryAction: { label: __('Return to institution selection'), action: goToSearch },
-        secondaryActions: { label: __('Get help'), action: gotToSupport },
+        secondaryActions: { label: __('Get help'), action: goToSupport },
       },
       [ACTIONABLE_ERROR_CODES.INSTITUTION_MAINTENANCE]: {
         title: __('Maintenance in progress'),
         primaryAction: { label: __('Return to institution selection'), action: goToSearch },
-        secondaryActions: { label: __('Get help'), action: gotToSupport },
+        secondaryActions: { label: __('Get help'), action: goToSupport },
       },
       [ACTIONABLE_ERROR_CODES.INSTITUTION_UNAVAILABLE]: {
         title: __('Unable to connect'),
         primaryAction: { label: __('Return to institution selection'), action: goToSearch },
-        secondaryActions: { label: __('Get help'), action: gotToSupport },
+        secondaryActions: { label: __('Get help'), action: goToSupport },
       },
     }),
     [dispatch],
