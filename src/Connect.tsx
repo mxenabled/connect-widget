5c83519af58b829c4ff8d46c2502ed4928f9621d
Jameson Brown <jameson.brown@mx.com>
2025-10-06 13:13:11 -0600

    feat(usenavigationpostmessage): implement useNavigationPostMessage hook for handling back navigation

--- src/Connect.tsx
+++ src/Connect.tsx
@@ -35,6 +35,7 @@ import { POST_MESSAGES } from 'src/const/postMessages'
 import PostMessage from 'src/utilities/PostMessage'
 import type { RootState } from 'reduxify/Store'
 import useLoadConnect from 'src/hooks/useLoadConnect'
+import { useNavigationPostMessage } from 'src/hooks/useNavigationPostMessage'
 import { PostMessageContext } from 'src/ConnectWidget'
 
 type ConnectState = {
@@ -74,6 +75,7 @@ export const Connect: React.FC<ConnectProps> = ({
 
   ...props
 }) => {
+  useNavigationPostMessage()
   const connectConfig = useSelector(selectConnectConfig)
   const initialConfig = useSelector(selectInitialConfig)
   const loadError = useSelector((state: RootState) => state.connect.loadError)
@@ -137,11 +139,6 @@ export const Connect: React.FC<ConnectProps> = ({
   }, [])
 
   useEffect(() => {
-    // When Connect is being used within our SDK's, we need to support an OS level back action
-    // We listen to a specific event to handle our postMessage callback with our `did_go_back` boolean
-    // To allow the host to know if we are still within Connect or to close it.
-    window.addEventListener('message', _handleNavigationPostMessage)
-
     loadConnect(props.clientConfig)
     dispatch(loadProfiles(props.profiles))
     dispatch(loadUserFeatures(props.userFeatures))
@@ -158,7 +155,6 @@ export const Connect: React.FC<ConnectProps> = ({
     }
     return () => {
       dispatch(connectActions.resetConnect())
-      window.removeEventListener('message', _handleNavigationPostMessage)
     }
   }, [])
 
@@ -214,38 +210,6 @@ export const Connect: React.FC<ConnectProps> = ({
       loadConnect(props.clientConfig)
     }
   }, [isLoading, step, props.clientConfig])
-
-  const _handleNavigationPostMessage = (event: MessageEvent) => {
-    const eventData = PostMessage.parse(event.data)
-    if (eventData.type === 'mx/navigation') {
-      // Specifically looking for the 'back' action, in the future we could support others
-      if (eventData.payload.action === 'back') {
-        const dontChangeConnectStep =
-          step === STEPS.SEARCH ||
-          step === STEPS.DISCLOSURE ||
-          step === STEPS.VERIFY_EXISTING_MEMBER ||
-          connectConfig.disable_institution_search
-
-        if (dontChangeConnectStep) {
-          // We consider SEARCH and DISCLOSURE to be as far back as Connect goes.
-          // Loaded in verify mode, we show VERIFY_EXISTING_MEMBER as the root.
-          // If disable_institution_search is `true`, we do not show the SEARCH step.
-          // If any of those conditions are met, we do not change the step when a back navigation event is received.
-          // Communicate that we did not go back to the SDK via the `did_go_back` payload.
-          postMessageFunctions.onPostMessage('navigation', { did_go_back: false })
-        } else {
-          // We want to reset connect by taking us back to the SEARCH or VERIFY_EXISTING_MEMBER step depending on config
-          dispatch({
-            type: connectActions.ActionTypes.GO_BACK_POST_MESSAGE,
-            payload: initialConfig,
-          })
-
-          // And communicating that we did go back to the SDK
-          postMessageFunctions.onPostMessage('navigation', { did_go_back: true })
-        }
-      }
-    }
-  }
 
   const _handleAddManualAccountClick = () => {
     dispatch(connectActions.stepToAddManualAccount())
