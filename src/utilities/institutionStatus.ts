4e3a4bd78e70c5373b485b033c9eca8a6dc94cd7
Logan Rasmussen <logan.rasmussen@mx.com>
2025-10-30 16:18:27 -0600

    feat: add the UNAVAILABLE institution status page

--- src/utilities/institutionStatus.ts
+++ src/utilities/institutionStatus.ts
@@ -1,4 +1,5 @@
 import { institutionIsBlockedForCostReasons } from './institutionBlocks'
+import { __ } from 'src/utilities/Intl'
 
 export const InstitutionStatus = {
   BLOCKED: 'BLOCKED',
@@ -26,6 +27,43 @@ export function getBlockedInstitutions() {
       guid: 'INS-78c7b591-6512-9c17-b092-1cddbd3c85ba', // PROD INS guid
     },
   ]
+}
+
+export function useInstitutionStatusMessage(institution: {
+  guid?: string | null
+  is_disabled_by_client?: boolean | null
+  name?: string | null
+}) {
+  const status = getInstitutionStatus(institution)
+
+  switch (status) {
+    case InstitutionStatus.BLOCKED:
+      // This is Chase specific messaging, until we fully design for blocking.  The problem is blocking is used for more than just Chase's blocking for cost reasons.
+      // So in the widget we are going to ony show blocking for Chase.
+      return {
+        title: __('Free %1 Connections Are No Longer Available', institution.name),
+        body: __(
+          '%1 now charges a fee for us to access your account data. To avoid passing that cost on to you, we no longer support %1 connections.',
+          institution.name,
+        ),
+      }
+    case InstitutionStatus.UNAVAILABLE:
+      // This is PayPal specific messaging for now...  Because we don't actually have a backend feature that
+      // tells us when an institution is unavailable.  The widget is essentially choosing to make PayPal unavailable.
+      // This does limit the unavailable to widget implementations for now.
+      return {
+        title: __('Connection not supported by %1', institution.name),
+        body: __(
+          "%1 currently limits how your data can be shared. We'll enable this connection once %1 opens access.",
+          institution.name,
+        ),
+      }
+    default:
+      return {
+        title: '',
+        body: '',
+      }
+  }
 }
 
 export function getInstitutionStatus(institution: {
@@ -57,6 +95,7 @@ export function isUnavailable(institution: {
   )
 }
 
+// Note - Blocked as of today means Chase blocking for cost reasons
 export function isBlocked(institution: {
   guid?: string | null
   is_disabled_by_client?: boolean | null
