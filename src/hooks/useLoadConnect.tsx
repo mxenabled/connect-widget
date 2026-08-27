6b002a5374303f5ecda37f0277ac884a82b23a12
Clement Mwimo <clement.mwimo@mx.com>
2026-08-27 13:34:37 -0600

    remove more unused code

--- src/hooks/useLoadConnect.tsx
+++ src/hooks/useLoadConnect.tsx
@@ -15,7 +15,7 @@ import { COMBO_JOB_DATA_TYPES } from 'src/const/comboJobDataTypes'
 import { VERIFY_MODE } from 'src/const/Connect'
 import { useApi, ApiContextTypes } from 'src/context/ApiContext'
 import { __ } from 'src/utilities/Intl'
-import type { RootState, AppDispatch } from 'src/redux/Store'
+import type { RootState } from 'src/redux/Store'
 import { instutionSupportRequestedProducts } from 'src/utilities/Institution'
 import { getExperimentalFeatures } from 'src/redux/reducers/experimentalFeaturesSlice'
 
@@ -53,7 +53,7 @@ const useLoadConnect = () => {
     return document.querySelector('html')?.getAttribute('lang') || 'en'
   }, [document.querySelector('html')?.getAttribute('lang')])
   const [config, setConfig] = useState<ClientConfigType>({} as ClientConfigType)
-  const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch()
 
   const loadConnect = useCallback((config: ClientConfigType) => setConfig(config), [config])
 
